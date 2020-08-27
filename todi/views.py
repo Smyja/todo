d53d69ea130de799676c11595767dd7a8c261a14
Smyja <akposlive59@gmail.com>
2020-08-27 10:07:41 +0100

    Added task context to views

--- todi/views.py
+++ todi/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,'toditemp/todi.html')
+	tasks=Task.objects.all()
+	context={'tasks':tasks}
+    return render(request,'toditemp/todi.html',context)
