356a01b4bb942fe6f25bceb747bfcdc46e7970ad
smyja <akposlive59@gmail.com>
2020-08-27 10:13:21 +0100

    added task template

--- todi/views.py
+++ todi/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 # Create your views here.
 
 def index(request):
-	tasks=Task.objects.all()
-	context={'tasks':tasks}
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
     return render(request,'toditemp/todi.html',context)
